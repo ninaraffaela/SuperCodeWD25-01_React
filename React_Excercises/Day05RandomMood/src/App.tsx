ad1dc64caa2c9409d0b51e6785936310475147cf
ninaraffaela <nina.r.ziegler@gmail.com>
2025-03-27 15:48:41 +0100

    added moods.lenght for random

--- React_Excercises/Day05RandomMood/src/App.tsx
+++ React_Excercises/Day05RandomMood/src/App.tsx
@@ -1,28 +1,32 @@
 import "./App.css";
 import { useState } from "react";
-import { Planet } from 'react-kawaii';
+import { Planet } from "react-kawaii";
 
-
-const moods = ["sad", "shocked", "happy", "blissful", "lovestruck", "excited", "ko"]
+const moods = [
+  "sad",
+  "shocked",
+  "happy",
+  "blissful",
+  "lovestruck",
+  "excited",
+  "ko",
+] as const;
 
 function App() {
-  
   const [moodNumber, setMoodNumber] = useState(2); //default mood. 0 = sad, 2 =happy
   const generateMood = () => {
-    const generateRandomNumber = Math.floor( Math.random() * 7 );
-    setMoodNumber(generateRandomNumber)  
-  } 
+    const generateRandomNumber = Math.floor(Math.random() * moods.length);
+    setMoodNumber(generateRandomNumber);
+  };
 
   console.log(moods[moodNumber]);
-  
- 
 
   return (
-  <>
-  <Planet size={200} mood={moods[moodNumber]} color="#FDA7DC" />
-  <button onClick={generateMood} > Change Mood </button>
-  </>
-  )
+    <>
+      <Planet size={200} mood={moods[moodNumber]} color="#FDA7DC" />
+      <button onClick={generateMood}> Change Mood </button>
+    </>
+  );
 }
 
 export default App;
